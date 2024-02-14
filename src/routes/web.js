8f7eb760f6ae45c491f5c2e1d26cbcc47a8e4cd5
Ivan San Martin <ivansanmartin987@gmail.com>
2024-02-14 00:31:54 -0300

    fix: remove endpoint

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,38 +1,20 @@
-const express = require("express")
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-const router = express.Router()
-
-console.log()
-
-router.get("/files/:filename", (req, res) => {
-    const filename = req.params.filename
-    const path = `/src/files/${filename}`
-    if (fs.existsSync(path)) {
-        res.contentType("application/pdf");
-        fs.createReadStream(path).pipe(res)
-    } else {
-        res.status(500)
-        res.send('File not found')
-    }
-
-})
-
+const router = express.Router();
 
 router.get("/projects/ivsm-panel", (req, res) => {
-    res.render("ivsm-panel/index.ejs")
-
-})
+    res.render("ivsm-panel/index.ejs");
+});
 
 router.get("/projects/ivsm-panel/display", (req, res) => {
-    res.render("ivsm-panel/panel-layout/pages/panel.ejs")
-})
+    res.render("ivsm-panel/panel-layout/pages/panel.ejs");
+});
 
 router.get("/projects/ivsm-panel/download", (req, res) => {
-    res.download("src/files/panel.zip")
-})
-
+    res.download("src/files/panel.zip");
+});
 
 module.exports = {
-    web: router
-}
+    web: router,
+};
